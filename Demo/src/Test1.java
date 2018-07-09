bf41c90f6a4bb9af40f48e0ae94e9ad62124bb79
yjp <1481443209@qq.com>
2018-07-09 12:09:25 +0800

    26565235

---
+++ Demo/src/Test1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Test1 {
+	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("大野请输入");
+//		int i = sc.nextInt();
+//	    System.out.println("大野输入的是"+i);
+//	    System.out.println("大野输入的是"+i);
+//	    System.out.println("大野输入的是"+i);
+//	    
+		//System.out.println((char)25105);
+//		System.out.println((char)29233);
+//		System.out.println((char)20320);
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println("请输入第一个数");
+//		int i=sc.nextInt();
+//		System.out.println("请输入第二个数");
+//		int a=sc.nextInt();
+//		System.out.println("请输入第三个数");
+//		int s=sc.nextInt();
+//		int max=(i>a)?((i>s)?(i):(s)):((a>s)?(a):(s));
+//		System.out.println("三个数值最大的为"+max);
+		System.out.println('0'+0);
+	}
+}
